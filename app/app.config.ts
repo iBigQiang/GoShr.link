a190b2cc4baab7f2f22ecd45c2c7a8d5fa16f9ef
iBigQiang <ibigqiang@gmail.com>
2026-02-08 01:13:03 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
-  title: 'Sink',
+  title: 'GoShr.link',
   github: 'https://github.com/miantiao-me/sink',
   coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  twitter: 'https://goshr.link/x',
+  telegram: 'https://goshr.link/telegram',
+  description: '简单、快速、安全的url链接缩短工具，可进行点击量统计分析，100% 免费部署在 Cloudflare 上运行无需传统服务器。',
+  image: 'https://goshr.link/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
